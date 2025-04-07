f8cf8a9172723be25364511ed1b6735d345874e7
Uday Maddeni <maddeniuday@gmail.com>
2025-04-07 15:28:31 +0530

    script updated

--- task06/modules/webapp/main.tf
+++ task06/modules/webapp/main.tf
@@ -16,8 +16,3 @@ resource "azurerm_linux_web_app" "app" {
 
   tags = var.tags
 }
-
-output "app_hostname" {
-  description = "Hostname of the Linux Web App"
-  value       = azurerm_linux_web_app.app.default_hostname
-}
